6e149467f572829c984ab7f701b8a388021ea73a
Lucas Hemmerlé <lucas.hemmerle@epitech.eu>
2020-10-23 18:31:04 +0900

    Started player implementation

---
+++ Assets/Scripts/Managers/GameSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetup : MonoBehaviour {
+    public enum StartCondition { InShip, OnBody }
+
+    public AstronomicalObject startBody;
+
+    void Start() {
+        PlayerController player = FindObjectOfType<PlayerController>();
+        player.SetVelocity(startBody.initialVelocity);
+    }
+}
