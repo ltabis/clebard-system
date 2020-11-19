61d2d7cb7bb5bbb41333a7595cb63734cf0a1d0f
carlLebrun <36409091+carlLebrun@users.noreply.github.com>
2020-11-19 05:06:16 +0100

    Classes small rework

--- Assets/Scripts/Enemy-AI/Peaceful.cs
+++ Assets/Scripts/Enemy-AI/Peaceful.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Peaceful : Mobile
 {
-    protected override void Wander()
-    {
-        if (isScared)
-            Scared();
-        else if (isCharmed)
-            Charmed();
-        else
-            Patroling();
-    }
+
 }
