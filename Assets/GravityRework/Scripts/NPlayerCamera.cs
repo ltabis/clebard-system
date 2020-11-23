be1af24f7451c3f24a77232d87e6c1d5ff326e35
ltabis <tab.rysak70@gmail.com>
2020-11-23 11:21:15 +0900

    :truck: moved file into appropriate location. 	added a basic controller. 	the camera follows a focus point.

---
+++ Assets/GravityRework/Scripts/NPlayerCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class NPlayerCamera : MonoBehaviour
+{
+    // focus on an object.
+    [SerializeField]
+	Transform focus = default;
+    // distance from the focused object.
+	[SerializeField, Range(1f, 20f)]
+	float distance = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        // relocating the camera at the desired distance from the focus point.
+        transform.localPosition = focus.position - transform.forward * distance;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GravityRework/PlayerCamera.cs
+++ Assets/Scripts/GravityRework/PlayerCamera.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class PlayerCamera : MonoBehaviour
 {
+    // object to focus.
+    [SerializeField]
+    private Transform focus;
+    // distance from the object.
+    [SerializeField, Range(1f, 20f)]
+    private float distance = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
