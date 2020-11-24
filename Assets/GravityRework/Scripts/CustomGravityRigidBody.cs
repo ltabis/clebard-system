bdac4885c88cb8cfe31236b690a4cc2c3bbafaba
ltabis <tab.rysak70@gmail.com>
2020-11-25 05:47:52 +0900

    :sparkles: added a script to apply custom gravity to world objects.

---
+++ Assets/GravityRework/Scripts/CustomGravityRigidBody.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravityRigidBody : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(CustomGravity.GetGravity(rb.position), ForceMode.Acceleration);
+    }
+}
