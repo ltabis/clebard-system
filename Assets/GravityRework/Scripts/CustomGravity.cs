5d1328ecc351e3b2e4dd89e0f62cc44435650700
ltabis <tab.rysak70@gmail.com>
2020-11-25 16:46:12 +0900

    :sparkles: starting to create custom gravity source elements.

--- Assets/GravityRework/Scripts/CustomGravity.cs
+++ Assets/GravityRework/Scripts/CustomGravity.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public static class CustomGravity
 {
+    public static List<GravitySource> sources = new List<GravitySource>();
+
     public static Vector3 GetGravity(Vector3 position)
     {
-        return position.normalized * Physics.gravity.y;
+        Vector3 gravitySum = Vector3.zero;
+
+        // accumulating all gravities in the scene.
+        foreach (var gravity in sources)
+            gravitySum += gravity.GetGravity(position);
+
+        return gravitySum;
     }
 
     public static Vector3 GetGravity(Vector3 position, out Vector3 worldUp)
     {
-        worldUp = GetWorldUp(position);
-        return GetGravity(position);
+        Vector3 gravitySum = GetGravity(position);
+
+        worldUp = -gravitySum.normalized;
+        return gravitySum;
     }
 
     public static Vector3 GetWorldUp(Vector3 position)
     {
-        return position.normalized;
+        return -GetGravity(position).normalized;
+    }
+
+    public static void RegisterGravitySource(GravitySource source)
+    {
+        Debug.Assert(
+            !sources.Contains(source),
+            "Trying to add a gravity source already registered.",
+            source
+        );
+        sources.Add(source);
+    }
+    public static void UnregisterGravitySource(GravitySource source)
+    {
+        Debug.Assert(
+            sources.Contains(source),
+            "Trying to remove a gravity source that was not registered.",
+            source
+        );
+        sources.Remove(source);
+    }
+
+    public static void ChangeGravitySourceState(uint index, bool status)
+    {
+        if (index < sources.Count)
+            sources[(int)index].isEnabled = status;
     }
 }
---
+++ Assets/GravityRework/Scripts/GravitySource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySource : MonoBehaviour
+{
+    public bool isEnabled = true;
+
+    public virtual Vector3 GetGravity(Vector3 position)
+    {
+        return Physics.gravity;
+    }
+
+    void OnEnable()
+    {
+        CustomGravity.RegisterGravitySource(this);
+    }
+
+    void OnDisable() 
+    {
+        CustomGravity.UnregisterGravitySource(this);
+    }
+}
