c47659bcd88fd9f1a4898608d1892d1e1f2356a2
ltabis <tab.rysak70@gmail.com>
2020-11-25 15:09:04 +0900

    :arrow_up: preventing object affected by gravity to be in perpetual movement.

--- Assets/GravityRework/Scripts/CustomGravityRigidBody.cs
+++ Assets/GravityRework/Scripts/CustomGravityRigidBody.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CustomGravityRigidBody : MonoBehaviour
 {
-    Rigidbody rb;
+    [SerializeField]
+    bool isAbleToSleep = false;
+    private Rigidbody rb;
+    // delay from wich the object isn't moving but is maybe floating.
+    private float delay = 0f;
 
     void Awake()
     {
@@ -14,6 +18,23 @@ public class CustomGravityRigidBody : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isAbleToSleep) {
+            // prevent PhysicX to overwork.
+            if (rb.IsSleeping()) {
+                delay = 0f;
+                return;
+            }
+
+            // check if the object is moving.
+            if (rb.velocity.magnitude < 0.001f) {
+                delay += Time.deltaTime;
+                if (delay >= 1f)
+                    return;
+            } else
+                delay = 0f;
+        }
+
+        // add force following the current custom gravity direction.
         rb.AddForce(CustomGravity.GetGravity(rb.position), ForceMode.Acceleration);
     }
 }
