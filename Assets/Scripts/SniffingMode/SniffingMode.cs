3472e25b16243d8fd111eafff9bc9662ea2167ce
Lucas Hemmerlé <lucas.hemmerle@epitech.eu>
2020-11-28 17:48:58 +0900

    Added postprocessing effect for sniffing mode

---
+++ Assets/Scripts/SniffingMode/SniffingMode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class SniffingMode : MonoBehaviour {
+    private Camera mainCamera;
+    private UniversalAdditionalCameraData postProcessCamera;
+    public bool sniffingMode;
+
+    void Start() {
+        mainCamera = Camera.main;
+        postProcessCamera = mainCamera.GetComponent<UniversalAdditionalCameraData>();
+        sniffingMode = false;
+    }
+
+    void Update() {
+        SniffingModeCheck();
+    }
+
+    void SniffingModeCheck() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            if (sniffingMode)
+                postProcessCamera.renderPostProcessing = false;
+            else
+                postProcessCamera.renderPostProcessing = true;
+            sniffingMode = !sniffingMode;
+        }
+    }
+}
