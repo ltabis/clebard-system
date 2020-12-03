c518d105eca05cac36e2cb1696c4d525536c30a9
ltabis <tab.rysak70@gmail.com>
2020-12-03 21:34:01 +0900

    :twisted_rightwards_arrows: integrated the sniffing mode mechanic.

--- Assets/Scripts/SniffingMode/SniffingMode.cs
+++ Assets/Scripts/SniffingMode/SniffingMode.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class SniffingMode : MonoBehaviour {
-    private Camera mainCamera;
-    private Camera ppCamera;
+    public Camera mainCamera;
+    public Camera ppCamera;
     private UniversalAdditionalCameraData mainCameraURP;
     private UniversalAdditionalCameraData ppCameraURP;
-    public GameObject palmTree;
     public bool sniffingMode;
 
     void Start() {
-        mainCamera = Camera.main;
-        ppCamera = GameObject.FindGameObjectWithTag("PPCamera").GetComponent<Camera>();
         mainCameraURP = mainCamera.GetComponent<UniversalAdditionalCameraData>();
         ppCameraURP = ppCamera.GetComponent<UniversalAdditionalCameraData>();
         sniffingMode = false;
@@ -28,12 +25,10 @@ public class SniffingMode : MonoBehaviour {
             if (sniffingMode) {
                 mainCameraURP.renderPostProcessing = false;
                 ppCameraURP.renderPostProcessing = false;
-                palmTree.layer = LayerMask.NameToLayer("Default");
             }
             else {
                 mainCameraURP.renderPostProcessing = true;
                 ppCameraURP.renderPostProcessing = true;
-                palmTree.layer = LayerMask.NameToLayer("PostProcessing");
             }
             sniffingMode = !sniffingMode;
         }
