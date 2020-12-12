d850ef3fed84ab813aaadf055f1babf4cb21ad5d
ltabis <tab.rysak70@gmail.com>
2020-12-12 21:32:56 +0900

    :sparkles: added a objective system and a basic puzzle logic for the first scene.

---
+++ Assets/Scripts/Puzzles/IntroductionPuzzle/FindScraps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindScraps : MonoBehaviour
+{
+    [SerializeField]
+    private List<Scrap> scraps = new List<Scrap>();
+    [SerializeField]
+    private UIManager ui;
+
+    void Start()
+    {
+        ui.SetObjective("Find scraps. You can use your sniffing mode <A> to reveal important items.");
+    }
+
+   void Update()
+   {
+       checkFoundScraps();
+   }
+
+   void checkFoundScraps()
+   {
+        foreach (var scrap in scraps) {
+            if (!scrap.HasBeenFound())
+            return;
+        }
+
+        Debug.Log("All scraps found !");
+   }
+}
