37fa46fc2fbb380366ac944610734588c99e816d
ltabis <tab.rysak70@gmail.com>
2020-12-13 10:22:15 +0900

    :sparkles: added a spaceship to the main scene + added pulsating light script.

---
+++ Assets/Scripts/Environment/PulsatingLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsatingLight : MonoBehaviour
+{
+    [SerializeField]
+    private float pulseRange = 4f;
+    [SerializeField]
+    private float pulseSpeed = 3f;
+    [SerializeField]
+    private float pulseMinimum = 0f;
+
+    private Light lightSource;
+
+    void Start()
+    {
+        lightSource = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        lightSource.range = pulseMinimum + Mathf.PingPong(Time.time * pulseSpeed, pulseRange - pulseMinimum);
+    }
+}
