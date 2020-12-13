4cc78ea1238422f6b99c5dbe186967f9f69d677e
ltabis <tab.rysak70@gmail.com>
2020-12-13 12:22:40 +0900

    :arrow_up: added switches and logic to finish the level.

---
+++ Assets/Scripts/Puzzles/ActivatePlateformsPuzzle/ActivatePlateformsPuzzle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePlateformsPuzzle : Puzzle
+{
+    [SerializeField]
+    private List<Plateform> plateforms = new List<Plateform>();
+
+    void Update()
+    {
+        uint countActivatedPlaterforms = 0;
+
+        foreach (var plateform in plateforms)
+            if (plateform.IsActivated)
+                ++countActivatedPlaterforms;
+
+        if (countActivatedPlaterforms != plateforms.Count) {
+            managerRef.SetCurrentObjective("Switches", "Activate " + (plateforms.Count - countActivatedPlaterforms) + " more Transporter's switches.");
+        } else {
+            managerRef.RemoveCurrentObjective();
+            managerRef.NextPuzzle();
+        }
+    }
+
+    override public void OnLeavePuzzle() 
+    {
+        managerRef.RemoveCurrentObjective();
+    }
+
+    override public void OnStartPuzzle()
+    {
+        foreach (var plateform in plateforms)
+            plateform.EnablePlateform();
+
+        managerRef.SetCurrentObjective("Switches", "Activate " + plateforms.Count +  " more Transporter's switches");
+        managerRef.SetCurrentTip("Remember, you can use your Sniffing Mode <A> to reveal important items");
+    }
+}
---
+++ Assets/Scripts/Puzzles/ActivatePlateformsPuzzle/Plateform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plateform : MonoBehaviour
+{
+    [SerializeField]
+    private Light lightSource;
+    private bool isActivated = false;
+    private bool isDisabled = true;
+
+    void Awake()
+    {
+        lightSource.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDisabled)
+            return;
+
+        if (other.tag == "Player") {
+            isActivated = !isActivated;
+            if (isActivated)
+                lightSource.color = Color.green;
+            else
+                lightSource.color = Color.red;
+        }
+    }
+
+    public void EnablePlateform()
+    {
+        lightSource.enabled = true;
+        isDisabled = false;
+        lightSource.color = Color.red;
+    }
+
+    public bool IsActivated {
+        get {
+            return isActivated;
+        }
+    }
+}
