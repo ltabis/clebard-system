22a152e17979c89412ef97300d3e06a681edac2c
ltabis <tab.rysak70@gmail.com>
2020-12-14 23:31:13 +0900

    :arrow_up: linked menu.

---
+++ Assets/Scripts/LaunchScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchScene : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLaunch;
+
+    public void OnLaunchScene()
+    {
+        SceneManager.LoadScene(sceneToLaunch);
+    }
+}
